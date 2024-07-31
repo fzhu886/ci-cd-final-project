a3d7b70ef6b36e6d128d1fd48fe8619037fbe199
Fan Zhu <fzhu@ualberta.ca>
2024-07-30 18:12:22 -0600

    Update routes.py, removed trailing white spaces on line 106

--- service/routes.py
+++ service/routes.py
@@ -103,7 +103,7 @@ def update_counters(name):
     app.logger.info("Request to Update counter: %s...", name)
     if name not in COUNTER:
         return abort(
-            status.HTTP_404_NOT_FOUND, 
+            status.HTTP_404_NOT_FOUND,
             f"Counter {name} does not exist"
         )
 
